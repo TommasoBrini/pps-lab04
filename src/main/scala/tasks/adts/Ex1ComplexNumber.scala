c62b9b7bc64d95c4fa05a7c129b29b1a1c19fee6
Tommaso <tommaso.brini@studio.unibo.it>
2025-03-24 10:54:23 +0100

    solved sum and substract exercise 1

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -19,15 +19,15 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
-    case class ComplexImpl(re: Double, im: Double)
+    private case class ComplexImpl(re: Double, im: Double)
     // Change assignment below: should probably define a case class and use it?
     opaque type Complex = ComplexImpl
 
     def complex(re: Double, im: Double): Complex = ComplexImpl(re, im)
 
-    extension (complex: Complex)
-      def re(): Double = complex.re
-      def im(): Double = complex.im
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
+    extension (c: Complex)
+      def re(): Double = c.re
+      def im(): Double = c.im
+      def sum(other: Complex): Complex = complex(c.re + other.re, c.im + other.im)
+      def subtract(other: Complex): Complex = complex(c.re - other.re, c.im - other.im)
       def asString(): String = ???
