b19abb12755f0aab1ae9e697b90bfcb470385e6f
Tommaso <tommaso.brini@studio.unibo.it>
2025-03-24 16:59:58 +0100

    exercise 5 - solved exercise

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,8 @@
 package u04lab
-import u03.Sequences.* 
+import u03.Sequences.*
 import Sequence.*
+import u03.Optionals.*
+import Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -19,8 +21,34 @@ object Ex5Traversable:
 
   def log[A](a: A): Unit = println("The next element is: "+a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
+  /*def logAll[A](seq: Sequence[A]): Unit = seq match
     case Cons(h, t) => log(h); logAll(t)
     case _ => ()
+  */
+  def logAll[T[_]: Traversable, A](ta: T[A]): Unit =
+    val traversable = summon[Traversable[T]]
+    traversable.foreach(ta)(log)
 
-  
+  trait Traversable[T[_]]:
+    def foreach[A](t: T[A])(f: A => Unit): Unit
+
+  given Traversable[Sequence] with
+    override def foreach[A](seq: Sequence[A])(f: A => Unit): Unit = seq match
+      case Cons(h, t) => f(h); foreach(t)(f)
+      case _ => ()
+
+  given Traversable[Optional] with
+    override def foreach[A](opt: Optional[A])(f: A => Unit): Unit = opt match
+      case Just(v) => f(v)
+      case _ => ()
+
+
+@main def tryTraversable =
+  val nums: Sequence[Int] = Cons(1, Cons(2, Cons(3, Nil())))
+  val optNum: Optional[Int] = Just(42)
+
+  import Ex5Traversable.*
+
+  logAll(nums)
+  logAll(optNum)
+
